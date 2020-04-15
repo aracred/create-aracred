5844bcd4536df9eec79a40d2b7c7c5bc86518630
pythonpete32 <pythonpete32@gmail.com>
2020-04-15 09:11:24 +0100

    writeTemplate to project logic

--- src/te.js
+++ src/te.js
@@ -13,12 +13,16 @@ const userName = require('os').userInfo().username;
 const path = `/home/${userName}/aracred/`
 
 const addressbook = addressbookTemplate(config.get('identities'))
-const dao = daoTemplate
-const project = projectTemplate
-const weights = weightsTemplate
+const dao = daoTemplate(config.get('identities'))
+const project = projectTemplate(config.get('discordId'), config.get('identities'), config.get('repos'))
+const weights = weightsTemplate()
 
 // --------------------------------
 mkdirs()
 writeTemplate(`${path}config/addressbook.json`, addressbook)
+writeTemplate(`${path}config/dao.json`, dao)
+writeTemplate(`${path}config/project.json`, project)
+writeTemplate(`${path}config/weights.json`, weights)
+
 
 
