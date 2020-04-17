0b5bc7e69b398bf4533e2a6e6d77102220e0687d
Enrique Ortiz <hi@enriqueortiz.dev>
2020-04-17 02:46:37 -0400

    First working version

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
-const { spawn } = require('child_process')
+const chalk = require('chalk')
+const Configstore = require('configstore')
+const {
+  configureAracred,
+  getBotTemplate,
+  getGithubToken,
+  getDaoParams,
+  getDiscordParams,
+  getRepoTemplate,
+} = require('./handlers/handlers')
+const { useFiglet } = require('./utils')
 
-const ls = spawn('git', ['status'])
+// Init the config store and clear it
+const configStore = new Configstore('create-aracred')
+configStore.clear()
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`)
-})
+async function main() {
+  useFiglet('Aracred')
+  await getGithubToken(configStore)
+  await getDaoParams(configStore)
+  await getDiscordParams(configStore)
+  await getRepoTemplate()
+  await getBotTemplate()
 
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`)
-})
+  const {
+    TOKEN_MANAGER_ADDRESS: daoAddress,
+    DAO_ADDRESS: tokenManagerAddress,
+    VOTING_ADDRESS: votingAddress,
+    DAO_NETWORK: environment,
+    CHANNEL_NAME: channelName,
+    CHANNEL_ID: channelId,
+  } = configStore.all
 
-ls.on('error', (error) => {
-  console.log(`error: ${error.message}`)
-})
+  configureAracred({
+    daoAddress,
+    tokenManagerAddress,
+    votingAddress,
+    environment,
+    channelName,
+    channelId,
+  })
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`)
-})
+  console.log(chalk.blue('All done!'))
+}
+
+main()
+  .then(() => console.log(configStore.all))
+  .catch((err) => console.log(err))
